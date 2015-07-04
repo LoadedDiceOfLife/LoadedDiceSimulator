28ea42e85bfdd28240e03cb998820a51d048b088
Pamplemousse <xavier.maso@net-c.com>
2015-07-05 11:58:14 +1200

    Add bower to webpack to manage front libraries

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+var webpack = require("webpack");
+var BowerWebpackPlugin = require("bower-webpack-plugin");
+
 module.exports = {
     entry: "./js/entry.js",
     output: {
@@ -8,5 +11,9 @@ module.exports = {
         loaders: [
             { test: /\.css$/, loader: "style!css" }
         ]
-    }
+    },
+    plugins: [
+        new BowerWebpackPlugin(),
+        new webpack.ProvidePlugin()
+    ]
 };
