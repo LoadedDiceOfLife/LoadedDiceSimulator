00b2bdfa99c3ac4a86f3ff2a03436ced75b3fcd6
Pamplemousse <xavier.maso@net-c.com>
2015-07-05 12:08:41 +1200

    Rename folders to remove capital letters

--- js/entry.js
+++ js/entry.js
@@ -74,4 +74,13 @@ $(function() {
             $(".select-area").append('<option value=' + area.code + '>' + area.name + '</option>');
         });
     });
+
+
+    $(document).ready(function() {
+        $.getJSON('assets/strings/names.json').then(function(names) {
+            console.log(_.sample(names));
+        });
+        // $('.presentation-name').html = ''
+        // $('.presentation-description').html = ''
+    });
 });
